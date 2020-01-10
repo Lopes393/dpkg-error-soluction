b85aa40c13447da11e9c81c3308d1614a4eb7608
Murilo Lopes <mloopes11@gmail.com>
2020-01-10 10:41:25 -0300

    script soluction

---
+++ soluctdpkg.sh
@@ -0,0 +1,6 @@
+##Suluction of error dpkg E: Sub-process /usr/bin/dpkg returned an error code (1)
+
+sudo apt-get install -f
+sudo apt-get upgrade -f
+sudo apt-get dist-upgrade -f
+sudo dpkg --configure -a
